e4d9d5160abfa783dea85c59732bca809fcf9b91
NikhilAg2021 <78316627+NikhilAg2021@users.noreply.github.com>
2021-07-08 16:22:18 +0530

    Updated counter-set and get values from server

--- counter.js
+++ counter.js
@@ -1,16 +1,20 @@
-        let c=0;
+        if (!localStorage.getItem('c'))
+        {
+            localStorage.setItem('c',0);
+        }
         function count() 
         {
+            let c = localStorage.getItem('c')
             c++;     
             document.querySelector('h1').innerHTML=c;
-            if(c%10 === 0)
-            {
-                alert(`Counter is now at ${c}`)
-            }
+            localStorage.setItem('c', c)
         }
 
         document.addEventListener('DOMContentLoaded', function(){
+            document.querySelector('h1').innerHTML=localStorage.getItem('c');
             document.querySelector('button').onclick=count; ///run function 'COUNT '(NOT CALLING IT) when button clicked
-        })
+        
+            
+        });
         
   
